e8ede24ef36f35c4de5e472e3de07e0b13d851d3
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2013-04-12 16:28:50 +0900

    add interface ICodeMap.java

---
+++ src/main/java/com/lightspeedworks/codemap/ICodeMap.java
@@ -0,0 +1,35 @@
+/**
+ * character code map {文字コードマッピング}
+ */
+package com.lightspeedworks.codemap;
+
+/**
+ * character code map {文字コードマッピング}
+ *
+ * @author LightSpeedC (Kazuaki Nishizawa; 西澤 和晃)
+ */
+public interface ICodeMap {
+	/**
+	 * deletes character code mapping table {文字コードマッピングテーブル削除}
+	 */
+	public void clear();
+
+	/**
+	 * sets value to character code mapping table {文字コードマッピングに値を設定}
+	 *
+	 * @param index
+	 *            integer index {整数インデックス}
+	 * @param value
+	 *            integer value {整数値}
+	 */
+	public ICodeMap set(int index, int value);
+
+	/**
+	 * gets value from character code mapping table {文字コードマッピングの値を取得}
+	 *
+	 * @param index
+	 *            integer index {整数インデックス}
+	 * @return integer value {整数値}
+	 */
+	public int get(int index);
+}
